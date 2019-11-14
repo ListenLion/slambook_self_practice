7f9d3602af6fcc58497aad901c8cef1139671f9a
ListenLion <2297591181@qq.com>
2019-11-14 09:32:03 +0800

    commit slambook_self-practice ch2

---
+++ ch2/build/CMakeFiles/hellslam.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hellslam.dir/helloslam.cpp.o"
+  "hellslam.pdb"
+  "hellslam"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hellslam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
